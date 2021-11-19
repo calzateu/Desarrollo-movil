5dc9c64b7aaf842b3a4778647cc7905b8ac3f5c3
unknown <laura.caicedoprieto@gmail.com>
2021-11-19 16:24:57 -0500

    Configuración inicial, icono, splashscreen, home

---
+++ Celecta/app/src/main/java/com/mintic22/celecta/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mintic22.celecta.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mintic22.celecta.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
