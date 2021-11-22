00d8cdfcbc7a8be21bba9eb6fc46cc9c8e73bcfd
acostajohan <acostajohan.2@hotmail.com>
2021-11-22 17:12:43 -0500

    Se realizó la guía 12

--- Celecta/app/src/main/java/com/mintic22/celecta/view/ui/activities/MainActivity.kt
+++ Celecta/app/src/main/java/com/mintic22/celecta/view/ui/activities/MainActivity.kt
@@ -2,11 +2,27 @@ package com.mintic22.celecta.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mintic22.celecta.R
+import com.mintic22.celecta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configNav()
+    }
+
+    fun configNav(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnv_menu).setupWithNavController(navController)
     }
 }
